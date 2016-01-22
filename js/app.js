19494983c327a04e6ee8d3135e7bdc36c644a2fd
Allen Zhong <zhong.allen@gmail.com>
2016-01-22 20:28:35 +0800

    fix responsive layout

--- js/app.js
+++ js/app.js
@@ -6,12 +6,11 @@ window.onload = function() {
         document.body.classList.toggle("js-show-right-sidebar");
         var rightSiderbar = document.querySelector(".site__right-sidebar");
         rightSiderbar.classList.toggle("js-show-right-sidebar");
-
     });
 }
 
 function makeCartScrollNicely() {
   var cart = document.querySelector(".cart__content");
-  // Ps.initialize(cart);
+  Ps.initialize(cart);
 }
 
